25b2d0efd06d14186edfa9143119facdaa13da33
darren <764190362@qq.com>
2017-11-06 13:04:55 +0800

    iview, 响应式布局，结合偏移量

--- src/components/iview/ResponsiveLayout.vue
+++ src/components/iview/ResponsiveLayout.vue
@@ -5,5 +5,11 @@
             <Col :xs="20" :sm="16" :md="12" :lg="8"><div style="background-color:green;">Col</div></Col>
             <Col :xs="2" :sm="4" :md="6" :lg="8"><div style="background-color:blue;">Col</div></Col>
         </Row>
+        <br>
+        <Row>
+            <Col :xs="{span:5, offset:1}" :lg="{span:6, offset:2}"><div style="background-color:blue;">Col</div></Col>
+            <Col :xs="{span:11, offset:1}" :lg="{span:6, offset:2}"><div style="background-color:green;">Col</div></Col>
+            <Col :xs="{span:5, offset:1}" :lg="{span:6, offset:2}"><div style="background-color:blue;">Col</div></Col>
+        </Row>
     </div>
 </template>
